ae4b4b6167f97dbb0242068b2297f47daf33a75a
Bruno Capezzali <capezzbr@gmail.com>
2014-07-15 15:26:04 +0100

    Created and Interface as callback only the AndroidUIActionSheet

---
+++ src/com/brunocapezzali/AndroidUIActionSheet/components/OnWidgetClickListener.java
@@ -0,0 +1,9 @@
+package com.brunocapezzali.AndroidUIActionSheet.components;
+
+/**
+ * Created by brunocapezzali on 15/07/2014.
+ *
+ */
+public interface OnWidgetClickListener {
+    public void onClicked(BaseWidget widget);
+}
