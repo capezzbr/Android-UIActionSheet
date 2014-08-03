97bcbad361d84483544fa5a89579272f11184988
Bruno Capezzali <capezzbr@gmail.com>
2014-08-04 00:27:42 +0100

    SpaceWidget now can have a custom height

--- application/src/main/java/com/brunocapezzali/androiduiactionsheet/SpaceWidget.java
+++ application/src/main/java/com/brunocapezzali/androiduiactionsheet/SpaceWidget.java
@@ -1,17 +1,32 @@
 package com.brunocapezzali.androiduiactionsheet;
 
+import android.app.ActionBar;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.LinearLayout;
 
 /**
  * Created by Bruno Capezzali on 15/07/2014.
  */
 public class SpaceWidget extends BaseWidget {
 
-    public SpaceWidget(int tag) { super(tag, R.layout.space_widget); }
+    private boolean mOnlySpace = true;
+    private int mHeight;
+
+    public SpaceWidget(int height) {
+        super(R.layout.divider_widget);
+        init(true, height);
+    }
+
     public SpaceWidget() {
-        super(0, R.layout.space_widget);
+        super(R.layout.divider_widget);
+        init(false, 0);
+    }
+
+    private void init(boolean onlySpace, int height) {
+        mOnlySpace = onlySpace;
+        mHeight = height;
     }
 
     @Override
@@ -21,9 +36,25 @@ public class SpaceWidget extends BaseWidget {
 
         if ( mView == null ) {
             Context ctx = parent.getContext();
-            LayoutInflater layoutInflater = (LayoutInflater) ctx.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            LayoutInflater layoutInflater = (LayoutInflater)
+                    ctx.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             mView = layoutInflater.inflate(mResourceViewId, null);
+
+            if ( mOnlySpace ) {
+                LinearLayout container = (LinearLayout)mView.findViewById(R.id.container);
+                setHeightToLinearLayout(container, mHeight);
+
+                View lineView = mView.findViewById(R.id.viewLine);
+                lineView.setVisibility(View.GONE);
+            }
         }
         return mView;
+    }
+
+    private void setHeightToLinearLayout(LinearLayout layout, int height) {
+        LinearLayout.LayoutParams layoutParams =
+                (LinearLayout.LayoutParams) layout.getLayoutParams();
+        layoutParams.height = height;
+        layout.setLayoutParams(layoutParams);
     }
 }
